3cc7796274e2d827610034adecec1d2b63b571d8
sai9997 <146225257+sai9997@users.noreply.github.com>
2023-10-18 17:47:53 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,11 +4,9 @@ variable "aws_region" {
 }
 variable "AWS_ACCESS_KEY_ID" {
   type        = string
-  default     = ""
 }
 variable "AWS_SECRET_ACCESS_KEY" {
   type        = string
-  default     = ""
 }
 variable "Users" {
  type = list(string)
