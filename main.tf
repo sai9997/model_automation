21a20ba69a43d97943286a92872f7f7f26a917d0
sai9997 <sai.tech10k@gmail.com>
2023-10-20 16:50:54 -0500

    updated path

--- main.tf
+++ main.tf
@@ -18,6 +18,12 @@ resource "aws_iam_group" "GroupNames" {
   count = length(var.GroupNames)
   name = var.GroupNames[count.index]
 }
+
+resource "aws_iam_group" "mygroup" {
+  count = length(var.GroupNames)
+  name = "test"
+  
+}
 # resource "aws_subnet" "mysubnets" {
 #   vpc_id = vpc-09e26fd3d29fc79b1
 #   count = length(var.mysubnets)
