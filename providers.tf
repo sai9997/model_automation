8c3499864d54ce03e4e3df3d3270fede5c235465
sai9997 <sai.tech10k@gmail.com>
2023-10-20 18:11:19 -0500

    sai added

--- providers.tf
+++ providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.3.9"
 }
 provider "aws" {
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
+  # access_key = var.AWS_ACCESS_KEY_ID
+  # secret_key = var.AWS_SECRET_ACCESS_KEY
   region     = var.aws_region
 }
